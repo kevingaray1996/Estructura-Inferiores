41ddf2d96754ccc5ffddb395e005d017d521ff69
kevingaray1996 <kevingaray1996@gmail.com>
2026-07-30 20:28:37 -0300

    Update ListaPartidos.jsx

--- src/components/ListaPartidos.jsx
+++ src/components/ListaPartidos.jsx
@@ -190,8 +190,8 @@ function ListaPartidos({ categoriaId, categoriaNombre, onVolver, onElegirPartido
                     </p>
                   </div>
                   <div className="flex items-center gap-2">
-                      {p.link && (
-                      <a
+                    {p.link && (
+                      
                         href={p.link}
                         target="_blank"
                         rel="noreferrer"
