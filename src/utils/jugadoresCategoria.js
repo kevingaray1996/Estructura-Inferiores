cc9dd83bb74e3ce75b3fae1b7f37b6b8d6a81eeb
kevingaray1996 <kevingaray1996@gmail.com>
2026-08-03 22:38:33 -0300

    Update jugadoresCategoria.js

--- src/utils/jugadoresCategoria.js
+++ src/utils/jugadoresCategoria.js
@@ -3,14 +3,16 @@
 // ES la de Reserva. Así el cuerpo técnico de Reserva ve también a los
 // chicos de inferiores que bajaron a jugar con ellos.
 export function obtenerJugadoresDeCategoria(supabase, categoriaId, categorias, opciones = {}) {
-  const { select = '*', orderBy = 'apellido' } = opciones
+  const { select = '*', orderBy = 'apellido', soloDisponibles = false } = opciones
   const categoria = categorias?.find((c) => c.id === categoriaId)
-
   let query = supabase.from('jugadores').select(select)
   if (categoria?.es_reserva) {
     query = query.or(`categoria_id.eq.${categoriaId},tambien_reserva.eq.true`)
   } else {
     query = query.eq('categoria_id', categoriaId)
+  }
+  if (soloDisponibles) {
+    query = query.neq('estado', 'no_disponible')
   }
   if (orderBy) query = query.order(orderBy)
   return query
